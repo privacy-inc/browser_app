9249dd0765487090f8fa57021ff75d2d9794acf7
vaux <zero.griffin@gmail.com>
2020-12-12 10:23:13 +0100

    Use image

--- macOS/Window.swift
+++ macOS/Window.swift
@@ -15,8 +15,9 @@ final class Window: NSWindow, NSToolbarDelegate {
         toolbar!.showsBaselineSeparator = false
         
         toolbar!.insertItem(withItemIdentifier: search, at: 0)
-        toolbar!.centeredItemIdentifier = search
-        toolbar!.validateVisibleItems()
+//        toolbar!.centeredItemIdentifier = search
+//        toolbar!.validateVisibleItems()
+        toolbar!.displayMode = .iconAndLabel
         collectionBehavior = .fullScreenNone
         isReleasedWhenClosed = false
         setFrameAutosaveName("Window")
@@ -30,13 +31,21 @@ final class Window: NSWindow, NSToolbarDelegate {
     func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar: Bool) -> NSToolbarItem? {
         guard let item = toolbar.items.first(where: { $0.itemIdentifier == itemForItemIdentifier }) else {
             let item = NSToolbarItem(itemIdentifier: itemForItemIdentifier)
+            
             item.isEnabled = true
             item.autovalidates = true
             item.visibilityPriority = .high
             item.validate()
+            item.toolTip = "ssda"
+            item.title = "sda"
+            item.label = "fdsdasda"
+            item.paletteLabel = "fdffgdfd"
+            item.target = self
+            item.action = #selector(some)
+            item.image = NSImage(named: "ios")
             let search = NSSearchField(frame: .init(x: 0, y: 0, width: 100, height: 100))
             search.controlSize = .large
-            item.view = search
+//            item.view = search
             item.minSize.height = 100
             item.minSize.width = 40
             item.maxSize.height = 1000
@@ -52,5 +61,9 @@ final class Window: NSWindow, NSToolbarDelegate {
     
     func toolbarDefaultItemIdentifiers(_: NSToolbar) -> [NSToolbarItem.Identifier] {
         []
+    }
+    
+    @objc private func some() {
+        
     }
 }
