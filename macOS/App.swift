fa27b584254c4fd4ce01098fc3fc17dfdded7151
vaux <zero.griffin@gmail.com>
2020-12-22 17:12:14 +0200

    Add default browser

--- macOS/App.swift
+++ macOS/App.swift
@@ -28,6 +28,13 @@ import Sleuth
     func applicationWillFinishLaunching(_: Notification) {
         mainMenu = Menu()
         newWindow()
+        
+        NSAppleEventManager.shared().setEventHandler(
+            self,
+            andSelector: #selector(handle(_:_:)),
+            forEventClass: AEEventClass(kInternetEventClass),
+            andEventID: AEEventID(kAEGetURL)
+        )
     }
     
     @objc func newWindow() {
@@ -60,5 +67,15 @@ import Sleuth
     
     @objc func trackers() {
         (windows.first { $0 is Trackers } ?? Trackers()).makeKeyAndOrderFront(nil)
+    }
+    
+    @objc private func handle(_ event: NSAppleEventDescriptor, _ reply: NSAppleEventDescriptor) {
+        event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue?.removingPercentEncoding
+            .flatMap { URL(string: $0) }
+            .map {
+                let window = Window()
+                window.makeKeyAndOrderFront(nil)
+                window.browser.browse.send($0)
+        }
     }
 }
