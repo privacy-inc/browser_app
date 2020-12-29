8e38da31be8fdd7206406d112a54e8c70be91a86
vaux <zero.griffin@gmail.com>
2020-12-29 12:40:56 +0100

    Add targets

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by vaux on 29.12.20.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watch Extension/PrivacyApp.swift
@@ -0,0 +1,19 @@
+//
+//  PrivacyApp.swift
+//  watch Extension
+//
+//  Created by vaux on 29.12.20.
+//
+
+import SwiftUI
+
+@main
+struct PrivacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
