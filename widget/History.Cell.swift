c3696e0eaaf0f1b2fd58cab1c19170636a123244
vaux <zero.griffin@gmail.com>
2021-04-17 16:24:26 +0200

    Update widget

--- widget/History.Cell.swift
+++ widget/History.Cell.swift
@@ -7,21 +7,21 @@ extension History {
         
         var body: some View {
             Link(destination: page.url) {
-                HStack {
-                    VStack(alignment: .leading) {
-                        if !page.title.isEmpty {
-                            Text(verbatim: page.title)
-                                .font(.caption)
-                                .lineLimit(4)
-                        }
-                        Text(verbatim: page.subtitle)
-                            .font(.caption2)
-                            .foregroundColor(.init(.tertiaryLabel))
-                            .lineLimit(page.title.isEmpty ? 5 : 3)
+                VStack(alignment: .leading) {
+                    if !page.title.isEmpty {
+                        Text(verbatim: page.title)
+                            .font(.caption)
+                            .lineLimit(4)
                     }
-                    .fixedSize(horizontal: false, vertical: true)
-                    Spacer()
+                    Text(verbatim: page.subtitle)
+                        .font(.caption2)
+                        .foregroundColor(.init(.tertiaryLabel))
+                        .lineLimit(page.title.isEmpty ? 5 : 3)
                 }
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
+                .background(RoundedRectangle(cornerRadius: 12)
+                                .fill(Color.primary.opacity(0.03)))
             }
         }
     }
