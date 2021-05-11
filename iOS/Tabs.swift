a2bed55e4945969025773ad3d30e250dceb131c4
vaux <zero.griffin@gmail.com>
2021-05-11 17:35:21 +0200

    Update tabs

--- iOS/Tabs.swift
+++ iOS/Tabs.swift
@@ -2,8 +2,18 @@ import SwiftUI
 
 struct Tabs: View {
     @Binding var session: Session
+    let tabs: Namespace.ID
     
     var body: some View {
-        Circle()
+        VStack(spacing: 0) {
+            ScrollView(.horizontal) {
+                ForEach(0 ..< session.tab.items.count, id: \.self) {
+                    Item(session: $session, id: session.tab.items[$0].id)
+                        .matchedGeometryEffect(id: session.tab.items[$0].id, in: tabs)
+                        .padding()
+                }
+            }
+            Bar(session: $session)
+        }
     }
 }
