5b50aa3d72656ed015646724fc76f8ecec5ba883
vaux <zero.griffin@gmail.com>
2021-05-24 14:49:35 +0200

    Update search

--- iOS/Search.swift
+++ iOS/Search.swift
@@ -11,21 +11,30 @@ struct Search: View {
             Color(.secondarySystemBackground)
                 .edgesIgnoringSafeArea(.all)
             ScrollView {
-                Text("Bookmarks")
-                    .font(.footnote.bold())
-                    .foregroundColor(.accentColor)
-                    .padding([.top, .leading])
-                    .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
-                ForEach(bookmarks, id: \.self, content: cell)
-                Text("Recent")
-                    .font(.footnote.bold())
-                    .foregroundColor(.accentColor)
-                    .padding(.leading)
-                    .padding(.top, 30)
-                    .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
-                ForEach(recent, id: \.self, content: cell)
-                Spacer()
+                if bookmarks.isEmpty && recent.isEmpty {
+                    Image("blank")
+                        .padding(.top, 100)
+                        .frame(maxWidth: .greatestFiniteMagnitude, maxHeight: .greatestFiniteMagnitude)
+                }
+                if !bookmarks.isEmpty {
+                    Text("Bookmarks")
+                        .font(.footnote.bold())
+                        .foregroundColor(.accentColor)
+                        .padding([.top, .leading])
+                        .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
+                    ForEach(bookmarks, id: \.self, content: cell)
+                }
+                if !recent.isEmpty {
+                    Text("Recent")
+                        .font(.footnote.bold())
+                        .foregroundColor(.accentColor)
+                        .padding(.leading)
+                        .padding(.top, 30)
+                        .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
+                    ForEach(recent, id: \.self, content: cell)
+                }
             }
+            .animation(.spring(blendDuration: 0.3))
             Bar(session: $session, filter: $filter, id: id)
                 .frame(height: 0)
         }
