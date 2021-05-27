e50114d7776368358ee16a0d64d348271e3bd87b
vaux <zero.griffin@gmail.com>
2021-05-27 15:28:03 +0200

    Update bookmarks

--- macOS/New.Bookmarks.Cell.swift
+++ macOS/New.Bookmarks.Cell.swift
@@ -1,7 +1,16 @@
-import QuartzCore
+import AppKit
 
 extension New.Bookmarks {
     final class Cell: CollectionCell {
-        static let insets = CGFloat(4)
+        static let insets = CGFloat(10)
+        static let insets2 = insets * 2
+        
+        override var insets: CGFloat {
+            Self.insets
+        }
+    
+        override var highlighted: CGColor {
+            NSColor.windowBackgroundColor.cgColor
+        }
     }
 }
--- macOS/New.Bookmarks.swift
+++ macOS/New.Bookmarks.swift
@@ -1,26 +1,83 @@
 import AppKit
 import Combine
 import Archivable
+import Sleuth
 
 extension New {
-    private static let width = CGFloat(230)
-    static let padding = CGFloat(6)
-    static let horizontal = CGFloat(20)
-    static let top = CGFloat(20)
-    static let bottom = CGFloat(40)
-    static let delta = CGFloat(60)
-    static let margin = CGFloat(20)
-    static let url = 60
-    
     final class Bookmarks: Collection<New.Bookmarks.Cell> {
+        private static let padding = CGFloat(4)
+        private static let insets = CGFloat(6)
+        
         required init?(coder: NSCoder) { nil }
         init(id: UUID) {
             super.init()
             translatesAutoresizingMaskIntoConstraints = false
-            documentView!.layer!.backgroundColor = NSColor.unemphasizedSelectedTextBackgroundColor.cgColor
-            documentView!.layer!.cornerRadius = 12
             
+            let width = PassthroughSubject<CGFloat, Never>()
+            let info = PassthroughSubject<[CollectionItemInfo], Never>()
             
+            NotificationCenter
+                .default
+                .publisher(for: NSView.frameDidChangeNotification, object: contentView)
+                .compactMap {
+                    ($0.object as? NSView)?.bounds.width
+                }
+                .removeDuplicates()
+                .subscribe(width)
+                .store(in: &subs)
+            
+            Cloud
+                .shared
+                .archive
+                .map(\.bookmarks)
+                .removeDuplicates()
+                .map {
+                    $0
+                        .enumerated()
+                        .map { (index: Int, page: Page) in
+                            .init(
+                                id: index,
+                                string: .make {
+                                    if !page.title.isEmpty {
+                                        $0.add(page.title,
+                                               font: .preferredFont(forTextStyle: .body),
+                                               color: .labelColor)
+                                    }
+                                    if !page.access.domain.isEmpty {
+                                        if !page.title.isEmpty {
+                                            $0.linebreak()
+                                        }
+                                        $0.add(page.access.domain,
+                                               font: .preferredFont(forTextStyle: .callout),
+                                               color: .secondaryLabelColor)
+                                    }
+                                })
+                        }
+                }
+                .subscribe(info)
+                .store(in: &subs)
+            
+            info
+                .removeDuplicates()
+                .combineLatest(width
+                                .removeDuplicates())
+                .sink { [weak self] (info: [CollectionItemInfo], width: CGFloat) in
+                    let result = info
+                        .reduce(into: (items: Set<CollectionItem>(), y: Self.insets)) {
+                            let height = ceil($1.string.height(for: width - Cell.insets2) + Cell.insets2)
+                            $0.items.insert(.init(
+                                                info: $1,
+                                                rect: .init(
+                                                    x: Self.insets,
+                                                    y: $0.y,
+                                                    width: width,
+                                                    height: height)))
+                            $0.y += height + Self.padding
+                        }
+                    self?.items.send(result.items)
+                    self?.height.send(result.y + Self.insets)
+                }
+                .store(in: &subs)
         }
     }
 }
