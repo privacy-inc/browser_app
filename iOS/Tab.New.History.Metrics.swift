8fdb83928acf643a8fd66ece2b6959205c264d31
vaux <zero.griffin@gmail.com>
2021-06-16 13:23:23 +0200

    Fix history

--- iOS/Tab.New.History.Metrics.swift
+++ iOS/Tab.New.History.Metrics.swift
@@ -9,7 +9,7 @@ extension Tab.New.History {
         
         init(size: CGSize) {
             let aprox = size.width > 550 ? CGFloat(200) : 160
-            let total = size.width - spacing
+            let total = size.width - (spacing * 3)
             let width_spacing = aprox + spacing
             columns = Int(floor(total / width_spacing))
             width = aprox + (total.truncatingRemainder(dividingBy: width_spacing) / .init(columns))
