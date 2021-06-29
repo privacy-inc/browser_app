3d51348a104792af4f0f2d72bdf8a87c22ed3c2b
vaux <zero.griffin@gmail.com>
2021-06-29 12:16:01 +0200

    Fix autocomplete

--- iOS/Search.Autocomplete.Cell.swift
+++ iOS/Search.Autocomplete.Cell.swift
@@ -15,7 +15,7 @@ extension Search.Autocomplete {
                             .padding(.top, 4)
                     }
                     if !item.url.isEmpty {
-                        Text(verbatim: item.url)
+                        Text(verbatim: item.domain)
                             .foregroundColor(.secondary)
                             .padding(.bottom, 4)
                     }
